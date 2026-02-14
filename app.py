270e43dd56471df6d44a29137a45bf2340fbbe4c
Anirban Sen <anirbansen2709@gmail.com>
2026-02-15 02:34:38 +0530

    Add Streamlit and data processing libraries

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
